89864c048181a3790440736fe81d11135ef08623
Serzh Shem <serzh152nn@gmail.com>
2021-12-03 23:17:48 +0300

    hw

--- homework_5.js
+++ homework_5.js
@@ -142,9 +142,9 @@ console.log(initCap(str));
 // 11. Змеиный_регистр (snake_case)
 // Напишите функцию initSnake(str), которая преобразует стиль написания составных слов строки из CamelCase в snake_case,
 // при котором несколько слов разделяются символом подчеркивания (_), причём каждое слово пишется с маленькой буквы.
-console.log("Задание 11:");
+console.log("Задание 11: Не доделал");
 
-str = "HelloWorld";
+// str = "HelloWorld";
 
 // function initSnake(str) {}
 
@@ -213,3 +213,182 @@ function insert(str, substr, pos = 0) {
   return str.slice(0, pos) + substr + str.slice(pos, str.length);
 }
 console.log(insert(str, substr, 6));
+
+// 17. Ограничить длину строки
+// Напишите функцию limitStr(str, n, symb), которая обрезает строку, если она длиннее указанного количества символов n.
+// Усеченная строка должна заканчиваться троеточием «...» (если не задан параметр symb) или заданным символом symb.
+console.log("Задание 17:");
+
+let symb = "!!!";
+let n = 5;
+
+function limitStr(str, n, symb) {
+  if (symb === undefined) {
+    return `${str.slice(0, n)}${"..."}`;
+  } else if (symb !== undefined) {
+    return `${str.slice(0, n)}${symb}`;
+  }
+}
+
+console.log(limitStr(str, n, symb));
+
+// 18. Поделить строку на фрагменты
+console.log("Задание 18:");
+
+str = "Helloworld";
+
+function cutString(str, n) {
+  return str.slice(0, str.length / n);
+}
+
+console.log(cutString(str, 5));
+
+// 19. Количество вхождений символа в строке
+// Напишите функцию count(str, stringsearch), которая возвращает количество символов stringsearch в строке str.
+console.log("Задание 19:");
+
+(symb = "а"), (str = "Астрономия это наука о небесных объектах");
+
+function count(str, symb, stringsearch = 0) {
+  for (i = 0; i < str.length; i++) {
+    symb === str[i] ? (stringsearch += 1) : 0;
+  }
+  return stringsearch;
+}
+console.log(count(str, symb));
+
+// 20. Удалить лишние пробелы из строки
+// Напишите функцию strip(str), которая удаляет все лишние пробелы из строки str.
+console.log("Задание 20:");
+
+str = "    Max is a good      boy     ";
+
+function strip(str) {
+  return str.replace(/\s\s+/g, "");
+}
+console.log(strip(str));
+
+// 21. Удалить лишние слова из строки
+// Напишите функцию cutString(str, n), которая удаляет лишние слова из строки str, оставив в ней n слов.
+console.log("Задание 21:");
+
+str = "Сила тяжести приложена к центру масс тела";
+
+function cutString(str, n) {
+  return str.split(" ").slice(0, n).join(" ");
+}
+console.log(cutString(str, 3));
+
+// 22. Сортировка символов строки по алфавиту
+// Напишите функцию alphabetize(str), которая возвращает строку, отсортировав её символы в алфавитном порядке.
+
+console.log("Задание 22:");
+str = "Кактус";
+function alphabetize(str) {
+  return str.toUpperCase().split("").sort().join(" ");
+}
+console.log(alphabetize(str));
+
+// 23. Найти слово в строке
+// Напишите функцию findWord(word, str), которая проверяет, существует ли в строке str слова word.
+console.log("Задание 23:");
+
+str = "abc def ghi jkl mno pqr stu";
+let word = "ghi";
+
+function findWord(word, str) {
+  return str.split(" ").includes(word, 0);
+}
+
+console.log(findWord(word, str));
+
+// 24. Преобразовать строку в массив слов
+
+// Напишите функцию stringToarray(str), которая преобразует строку в массив слов.
+console.log("Задание 24:");
+
+str = "Каждый охотник желает знать";
+function stringToarray(str) {
+  return str.split(" ");
+}
+
+let arr = stringToarray(str);
+
+console.log(arr);
+
+// 25. Реализовать объект
+
+// 1.Создать объект с начальным полем title. Добавить поле с ключом description начальным значением строкой.
+// 2.Добавить метод addField, который при вызове добавляет новое поле в объект с переданным именем
+// и значением addField(fieldName, value), добавить метод editField(fieldName, value), который меняет указанное свойство объекта.
+//  Добавить метод удаление свойства deleteField(fieldName), удаляющий указанное поле.
+// Добавить и изменить поля с помощью этих методов.
+console.log("Задание 25: Не доделал");
+
+let someObj = {
+  title: "My object",
+};
+someObj.description = "";
+
+console.log(someObj);
+
+//26 Сделайте функцию getDigitsSum (digit - это цифра), которая параметром принимает целое число и возвращает сумму его цифр.
+console.log("Задание 26:");
+
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+num = 2020;
+
+getDigitsSum = (num) => {
+  return num.toString().split("").map(Number).reduce(reducer);
+};
+
+console.log(getDigitsSum(num));
+
+// 27. Найдите все года от 1 до 2020, сумма цифр которых равна 13.
+//  Для этого используйте вспомогательную функцию getDigitsSum из предыдущей задачи.
+console.log("Задание 27:");
+
+let numYears = 0;
+
+for (i = 1; i <= 2020; i++) {
+  if (getDigitsSum(i) === 13) {
+    numYears += 1;
+  }
+}
+console.log(numYears);
+
+console.log("Задание 28:");
+// 31. Написать функцию, которая принимает число от 1 до 7 и возвращает день недели. Решить с помощью объектов.
+console.log("Задание 31:");
+
+num = 5;
+
+someObj = {
+  Monday: "Понедельник",
+  Tuesday: "Вторник",
+  Wednesday: "Среда",
+  Thursday: "Четверг",
+  Friday: "Пятница",
+  Saturday: "Суббота",
+  Sunday: "Воскресенье",
+};
+
+let someFunc = (num) => {
+  if (num === 1) {
+    return someObj.Monday;
+  } else if (num === 2) {
+    return someObj.Tuesday;
+  } else if (num === 3) {
+    return someObj.Wednesday;
+  } else if (num === 4) {
+    return someObj.Thursday;
+  } else if (num === 5) {
+    return someObj.Friday;
+  } else if (num === 6) {
+    return someObj.Saturday;
+  } else if (num === 7) {
+    return someObj.Sunday;
+  }
+};
+
+console.log(someFunc(1));
